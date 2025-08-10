094f6aef00d440b481d599e0b18dc3a814db60b9
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-10 08:37:56 +0200

    Refactor flight search initialization and comment out destination code processing

--- flight_search.py
+++ flight_search.py
@@ -1,7 +1,35 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
+
+    def __init__(self):
+        self.api_key = os.getenv("AMADEUS_API_KEY")
+        self.api_secret = os.getenv("AMADEUS_API_SECRET")
+        self._token = self._get_new_token()
 
     def get_destination_code(self, city_name):
         # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
         code = "TESTING"
         return code
+
+    def _get_new_token(self):
+        header = {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+        body = {
+            "grant_type": "client_credentials",
+            "client_id": self.api_key,
+            "client_secret": self.api_secret,
+        }
+        response = requests.post(url="https://test.api.amadeus.com/v1/security/oauth2/token", headers=header, data=body)
+        print(f"Your token is {response.json()['access_token']}")
+        print(f"Your token expires in {response.json()['expires_in']} seconds")
+        return response.json()['access_token']
+        
